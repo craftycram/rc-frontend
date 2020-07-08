6b63d2180666dfa75566b6772f9c3b99d1d87d86
craftycram <github@craftycram.net>
2020-07-08 22:01:50 +0200

    added socket connection

--- src/main.js
+++ src/main.js
@@ -2,7 +2,14 @@ import '@babel/polyfill';
 import 'mutationobserver-shim';
 import Vue from 'vue';
 import './plugins/bootstrap-vue';
+import VueSocketIO from 'vue-socket.io';
 import App from './App.vue';
+
+Vue.use(new VueSocketIO({
+  debug: true,
+  connection: 'http://192.168.1.25:3001',
+  options: {},
+}));
 
 Vue.config.productionTip = false;
 
